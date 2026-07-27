ace69526a4f06f06499983f75f6af16e9e1383ed
Prajit Alexander <pcapcapca1@gmail.com>
2026-07-27 18:18:39 -0500

    Fix formatting in project description comment

--- Java Projects/Information Retrieval System/Project2336F24.java
+++ Java Projects/Information Retrieval System/Project2336F24.java
@@ -1,7 +1,7 @@
 /**
   Project Description:
   Information retrieval systems allow users to enter keywords and retrieve articles that have those keywords associated with them.
-  For example, once a student named Yi Li wrote a paper called, Object Class Recognition using Images of Abstract Regions,"
+  For example, once a student named Yi Li wrote a paper called "Object Class Recognition using Images of Abstract Regions"
   and included the following keywords: `object recognition', `abstract regions', `mixture models', and `EM algorithm'.
   If someone does a search for all articles about the EM algorithm, this paper (and many others) will be retrieved.
 
